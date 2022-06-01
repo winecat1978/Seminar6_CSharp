74c9b446ac676decec2d3d36ee58e8f0c5dcdf99
Katherine_20 <mob1911494@gmail.com>
2022-06-01 20:42:50 +0300

    copying an array

---
+++ No45/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] FillArray(int length)
+{
+    Random rnd = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+
+int [] CopyingArray(int length, int [] Array)
+{
+    int [] copy = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        copy[i] = Array[i];
+    }
+    return copy;
+}
+
+
+Console.WriteLine("Введите размерность массива: ");
+int length = int.Parse(Console.ReadLine());
+int[] Array = FillArray(length);
+Console.WriteLine(string.Join(" ", Array));
+int [] result = CopyingArray(length, Array);
+Console.WriteLine(string.Join(" ", result));
