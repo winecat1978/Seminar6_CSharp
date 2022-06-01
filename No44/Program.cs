001396844723c6b29c98e3330ac584aece37829f
Katherine_20 <mob1911494@gmail.com>
2022-06-01 20:30:29 +0300

    fibonacci numbers without recursion

---
+++ No44/Program.cs
@@ -0,0 +1,40 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse(ValueFromConsole, out int num))
+        {
+            return num;
+        }
+        else 
+        {
+            Console.WriteLine("Вы ввели не число, попробуйте еще раз!");
+        }
+    }
+}
+
+int [] Fibonacci (int N)
+{
+    int [] array = new int[N];
+    for (int i = 0; i < 2; i++)
+    {
+        array[i] = i;
+    }
+    for (int i = 2; i > 1 && i < N; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array;
+}
+
+string message = "Введите число: ";
+int N = GetNumber(message);
+int [] result = Fibonacci(N);
+Console.WriteLine(string.Join(" ", result));
